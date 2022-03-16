fd9c2806907bd1c15b0207aa6abb8e5df60fb02d
J <93469760+humormint@users.noreply.github.com>
2022-03-16 20:17:10 +0900

    Add files via upload

---
+++ 채지훈_2차구현과제_6753/LDH_LION.js
@@ -0,0 +1,7 @@
+function openLogin(로그인창열고닫기) {
+	document.getElementById("login-modal").style.display = 로그인창열고닫기;
+}
+
+function loginCheck() {
+    document.getElementById("check-box").setAttribute("src", "LDH_LION_img/icon_check.png")
+}
